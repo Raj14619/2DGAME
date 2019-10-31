c9682fdd2b297cde8873c0b5f62a8451b10d58cb
Ray <51682830+Ray1500@users.noreply.github.com>
2019-10-31 17:48:06 +0000

    EPISODE 16
    EPISODE 16

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+    public float fullHealth;
+    public GameObject deathFX;
+
+    float currentHealth;
+
+    playerController controlMovement;
+
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = fullHealth;
+        controlMovement = GetComponent<playerController>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void addDamage(float damage)
+    {
+        if (damage <= 0)
+        {
+            return;
+        }
+
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            makeDead();
+        }
+
+    }
+
+    public void makeDead()
+    {
+        Instantiate(deathFX, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+
+}
